2134d6f1228326bc05fbea557c121a133fcc49a2
Mosunmola Owolade <mbowolade@gmail.com>
2023-09-21 00:41:11 +0100

    code refactoring

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,61 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-// import { db } from '../firebase_setup/firebase';
-// import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase_setup/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { women, men } from '../redux/WatchesSlice';
 
 
 const useFetch = () => {
   const dispatch = useDispatch();
 
-  // const [womenWatches, setWomenWatches] = useState([]);
-  // const [mensWatches, setMensWatches] = useState([]);
+  const [womenWatches, setWomenWatches] = useState([]);
+  const [mensWatches, setMensWatches] = useState([]);
 
   useEffect(() => {
 
-    fetch('https://dummyjson.com/products/category/mens-watches',
-      {
-        method: 'GET'
-      }
-    ).then(res => res.json())
-      .then(watches => {
-        dispatch(women(watches.products))
+    // fetch('https://dummyjson.com/products/category/mens-watches',
+    //   {
+    //     method: 'GET'
+    //   }
+    // ).then(res => res.json())
+    //   .then(watches => {
+    //     dispatch(women(watches.products))
 
 
-      }).catch(error => {
-        console.log(error);
-      });
+    //   }).catch(error => {
+    //     console.log(error);
+    //   });
 
-    fetch('https://dummyjson.com/products/category/mens-watches',
-      {
-        method: 'GET'
-      }
-    ).then(res => res.json())
-      .then(watches => {
-        dispatch(men(watches.products))
-
-
-      }).catch(error => {
-        console.log(error);
-      });
+    // fetch('https://dummyjson.com/products/category/mens-watches',
+    //   {
+    //     method: 'GET'
+    //   }
+    // ).then(res => res.json())
+    //   .then(watches => {
+    //     dispatch(men(watches.products))
 
 
+    //   }).catch(error => {
+    //     console.log(error);
+    //   });
 
-    //   const mensWatchesCollectionRef = collection(db, 'menswatches');
-    //   const womenWatchesCollectionRef = collection(db, 'womenwatches');
 
-    //   const myData = async () => {
-    //     const womenCollection = await getDocs(womenWatchesCollectionRef);
-    //     const menCollection = await getDocs(mensWatchesCollectionRef);
 
-    //     setWomenWatches(womenCollection.docs.map((doc) => ({ ...doc.data() })));
-    //     setMensWatches(menCollection.docs.map((doc) => ({ ...doc.data() })));
-    //   };
-    //   myData();
+    const mensWatchesCollectionRef = collection(db, 'menswatches');
+    const womenWatchesCollectionRef = collection(db, 'womenwatches');
 
-    //   dispatch(women(womenWatches))
-    //   dispatch(men(mensWatches));
-  }, []);
+    const myData = async () => {
+      const womenCollection = await getDocs(womenWatchesCollectionRef);
+      const menCollection = await getDocs(mensWatchesCollectionRef);
+
+      setWomenWatches(womenCollection.docs.map((doc) => ({ ...doc.data() })));
+      setMensWatches(menCollection.docs.map((doc) => ({ ...doc.data() })));
+    };
+    myData();
+
+    dispatch(women(womenWatches))
+    dispatch(men(mensWatches));
+  });
 
 
   return (
